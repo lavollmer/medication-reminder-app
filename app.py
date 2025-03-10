87a8c210bb66cb8dfbf4d5bea1125cf3c5010799
Laura <laura.e.vollmer@gmail.com>
2025-03-10 16:44:46 -0500

    Adding user login code

--- app.py
+++ app.py
@@ -5,22 +5,51 @@ import jwt
 import datetime
 
 app = Flask(_name_)
-app.config['SECRET_KEY'] = ''
+app.config["SECRET_KEY"] = ""
+
 
 # database connection that connects to SQLite database
 def get_db():
-    conn = sqlite3.connect('medication_reminders.db')
+    conn = sqlite3.connect("medication_reminders.db")
     return conn
 
-@app.route('/register', methods=['POST'])
+
+@app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    email = data['email']
-    password = generate_password_hash(data['password'])
+    email = data["email"]
+    password = generate_password_hash(data["password"])
 
+    # user information stored in the users table in database
     conn = get_db()
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO users (email, password) VALUES (?,?)", (email,password))
+    cursor.execute(
+        "INSERT INTO users (email, password) VALUES (?,?)", (email, password)
+    )
     conn.commit()
 
     return jsonify({"message": "USer registered successfully!"}), 201
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE email=?", (email,))
+    user = cursor.fetchone()
+
+    if user and check_password_hash(user[2], password):
+        token = jwt.encode(
+            {
+                "user_id": user[0],
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+            },
+            app.config["SECRET_KEY"],
+            algorithm="HS256",
+        )
+        return jsonify({"token": token}), 200
+    return jsonify({"message": "Invalid credentials!"}), 401
